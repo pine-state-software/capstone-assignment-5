d4080f15e92d49354f334a9f2ba7788d3efec6d5
Connor Bray <connor.bray@maine.edu>
2022-11-15 16:18:42 +0000

    one file for sorting algorithms

---
+++ sort/sort.py
@@ -0,0 +1,42 @@
+def bubble(_list):
+    '''
+        Docstring
+    '''
+
+    length = len(_list)
+    sorted = True
+
+    for i in range(length):
+
+        # Return if last element
+        if i+1 == length:
+            # Return sorted list if no elements were swapped
+            if (sorted):
+                return _list
+            # Call function again for nth passthrough if array elements were swapped
+            else:
+                return bubble(_list)
+
+        else:
+            # Swap elements if the first one is smaller
+            if _list[i] > _list[i+1]:
+                _list[i], _list[i+1] = _list[i+1], _list[i]
+                sorted = False
+
+def quick(_list):
+    '''
+        Docstring
+    '''
+    return
+
+def insertion(_list):
+    '''
+        Docstring
+    '''
+    return
+
+
+if __name__ == '__main__':
+    print(bubble([3,2,1,7,9,10,11,45,-1]))
+    print(bubble([0]))
+    print(bubble([2,0]))
